c70093924a46a4c98faac9fc876d44dd306efcab
Jatin Parab <jatinparab98@gmail.com>
2020-08-21 23:50:28 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Form } from "./pages/form";
 import { Submissions } from "./pages/submissions";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path={`${process.env.PUBLIC_URL}/submissions`}>
-            <Submissions />
-          </Route>
-          <Route path={`${process.env.PUBLIC_URL}/`}>
-            <Form />
-          </Route>
-        </Switch>
+        <Route exact path={`/`} component={Form}></Route>
+        <Route path={`/submissions`} component={Submissions}></Route>
       </div>
     </Router>
   );
