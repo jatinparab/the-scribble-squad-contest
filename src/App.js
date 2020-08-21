340d791456071799aaead5f92ac59143b1987149
Jatin Parab <jatinparab98@gmail.com>
2020-08-21 23:44:00 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
- <div>
-       
-
+      <div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/submissions">
+          <Route path={`${process.env.PUBLIC_URL}/submissions`}>
             <Submissions />
           </Route>
-          <Route path="/">
+          <Route path={`${process.env.PUBLIC_URL}/`}>
             <Form />
           </Route>
         </Switch>
